2e77d159e9921efcb0e011e9adaa8beafebda648
Dam2204 <soyoung4576@gmail.com>
2024-10-30 18:33:05 +0900

    daily_mission_4

---
+++ src/handler/index.js
@@ -0,0 +1,23 @@
+import { HANDLER_IDS } from '../constants/handlerIds.js';
+import initialHandler from './user/initial.handler.js';
+
+const handlers = {
+  [HANDLER_IDS.INITIAL]: {
+    handler: initialHandler,
+    protoType: 'initial.InitialPayload',
+  },
+};
+
+export const getHandlerById = (handlerId) => {
+  if (!handlers[handlerId]) {
+    throw Error();
+  }
+  return handlers[handlerId].handler;
+};
+
+export const gerProtoTypeNameByHandlerId = (handlerId) => {
+  if (!handlers[handlerId]) {
+    throw Error();
+  }
+  return handlers[handlerId].protoType;
+};
